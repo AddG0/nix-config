2c3076b71840f3bf1694994abb3f5827924762d1
addg <addgamer09@gmail.com>
2025-04-14 23:10:30 +0100

    Removed stuff I am not going to use on uk-box

--- home/primary/uk-box.nix
+++ home/primary/uk-box.nix
@@ -12,8 +12,6 @@
     common/core # required
 
     #################### Host-specific Optional Configs ####################
-    common/optional/helper-scripts
-    common/optional/jupyter-notebook
   ];
 
   home = {
