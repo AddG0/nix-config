28073ee56d3be2d1b036bb49cb1d24ad9a9b2bbb
addg <addgamer09@gmail.com>
2025-05-16 17:50:22 -0500

    Updated packages

--- home/primary/azuree.nix
+++ home/primary/azuree.nix
@@ -36,7 +36,7 @@
     common/optional/development/virtualization/lens.nix
     common/optional/nixos/vlc.nix
     common/optional/remote-desktop/mouseshare/lan-mouse.nix
-    common/optional/development/postman.nix
+    # common/optional/development/postman.nix
   ];
 
   home.file."Videos/Movies".source = config.lib.file.mkOutOfStoreSymlink "/mnt/videos";
