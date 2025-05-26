04b6e5282a2229249ab226f720a775f39c752fd6
addg <addgamer09@gmail.com>
2025-05-26 18:29:18 -0500

    Setup home manager on loki

--- hosts/common/optional/nixos/services/home-assistant.nix
+++ hosts/common/optional/nixos/services/home-assistant.nix
@@ -3,16 +3,61 @@
     enable = true;
     extraComponents = [
       # Components required to complete the onboarding
-      "esphome"
+      "analytics"
+      "google_translate"
       "met"
       "radio_browser"
+      "shopping_list"
+      # Recommended for fast zlib compression
+      # https://www.home-assistant.io/integrations/isal
+      "isal"
     ];
+    extraPackages = ps: with ps; [psycopg2];
     config = {
       # Includes dependencies for a basic setup
       # https://www.home-assistant.io/integrations/default_config/
       default_config = {};
+
+      recorder.db_url = "postgresql://@/hass";
+
+      # Setup home assistant for nginx reverse proxy
+      http = {
+        server_host = "::1";
+        trusted_proxies = ["::1"];
+        use_x_forwarded_for = true;
+      };
+
+      # Import the ui configurations
+      # "automation ui" = "!include automations.yaml";
+      # "scene ui" = "!include scenes.yaml";
+      # "script ui" = "!include scripts.yaml";
     };
   };
 
-  security.firewall.allowedTCPPorts = [8123];
+  services.nginx.virtualHosts."home-assistant.addg0.com" = {
+    forceSSL = true;
+    useACMEHost = "addg0.com";
+    extraConfig = ''
+      proxy_buffering off;
+    '';
+    locations."/" = {
+      proxyPass = "http://[::1]:8123";
+      proxyWebsockets = true;
+    };
+  };
+
+  services.postgresql = {
+    enable = true;
+    ensureDatabases = ["hass"];
+    ensureUsers = [
+      {
+        name = "hass";
+        ensureDBOwnership = true;
+      }
+    ];
+  };
+
+  networking.hosts = {
+    "127.0.0.1" = ["home-assistant.addg0.com"];
+  };
 }
