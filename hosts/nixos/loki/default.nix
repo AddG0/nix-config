6b131547a9570a00afd69e0de75ec8c49069a7ac
addg <addgamer09@gmail.com>
2025-05-26 21:23:54 -0500

    Turned on vscode server on loki

--- hosts/nixos/loki/default.nix
+++ hosts/nixos/loki/default.nix
@@ -20,15 +20,15 @@
     inputs.hardware.nixosModules.common-pc-ssd
 
     #################### Disk Layout ####################
-#    inputs.disko.nixosModules.disko
-#    (lib.custom.relativeToHosts "common/disks/btrfs-disk.nix")
-#    {
-#      _module.args = {
-#        # Use the full model name disk ID for the 2TB NVMe drive
-#        disk = "/dev/disk/by-id/nvme-Acer_SSD_N5000_2TB_ASBJ53410202076";
-#        withSwap = false;
-#      };
-#    }
+    #    inputs.disko.nixosModules.disko
+    #    (lib.custom.relativeToHosts "common/disks/btrfs-disk.nix")
+    #    {
+    #      _module.args = {
+    #        # Use the full model name disk ID for the 2TB NVMe drive
+    #        disk = "/dev/disk/by-id/nvme-Acer_SSD_N5000_2TB_ASBJ53410202076";
+    #        withSwap = false;
+    #      };
+    #    }
 
     #################### Misc Inputs ####################
 
@@ -40,6 +40,7 @@
       "common/optional/nixos/services/openssh.nix" # allow remote SSH access
       "common/optional/nixos/services/home-assistant.nix"
       "common/optional/nixos/services/nginx.nix" # nginx
+      "common/optional/nixos/services/vscode-server.nix"
     ])
   ];
 
