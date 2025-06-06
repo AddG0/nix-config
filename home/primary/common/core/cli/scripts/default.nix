af638e88fea67665178066e7acdb01af1dc34c72
addg <addgamer09@gmail.com>
2025-06-06 09:54:30 -0500

    Moved ns script to scripts dir

--- home/primary/common/core/cli/scripts/default.nix
+++ home/primary/common/core/cli/scripts/default.nix
@@ -1,3 +1,20 @@
 # TODO: add more scripts here
 {
+  outputs,
+  pkgs,
+  ...
+}: let
+  ns = pkgs.writeShellApplication {
+    name = "ns";
+    runtimeInputs = with pkgs; [
+      nix-search-tv # Required by nixpkgs.sh
+      fzf # Required by nixpkgs.sh
+      gawk # Required by nixpkgs.sh
+    ];
+    text = builtins.readFile ./nixpkgs.sh;
+  };
+in {
+  home.packages = [
+    ns
+  ];
 }
