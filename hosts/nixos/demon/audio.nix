894755db82315b877794c31bf618c3cecd840cd7
addg <addgamer09@gmail.com>
2025-10-16 21:35:02 -0500

    Added demon host

---
+++ hosts/nixos/demon/audio.nix
@@ -0,0 +1,98 @@
+{pkgs, ...}: {
+  security.pam.loginLimits = [
+    {
+      domain = "@audio";
+      type = "soft";
+      item = "rtprio";
+      value = "95";
+    }
+    {
+      domain = "@audio";
+      type = "hard";
+      item = "rtprio";
+      value = "99";
+    }
+    {
+      domain = "@audio";
+      type = "soft";
+      item = "memlock";
+      value = "unlimited";
+    }
+    {
+      domain = "@audio";
+      type = "hard";
+      item = "memlock";
+      value = "unlimited";
+    }
+  ];
+
+  # Enable sound with pipewire.
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    wireplumber.enable = true;
+    # Uncomment if you use JACK applications
+    # jack.enable = true;
+
+    # Configure PipeWire to support high sample rates
+    extraConfig.pipewire = {
+      "99-hifi.conf" = {
+        "context.properties" = {
+          "default.clock.rate" = 96000;
+          "default.clock.allowed-rates" = [44100 48000 88200 96000 192000];
+          "resample.quality" = 10;
+        };
+      };
+      "99-input-denoising.conf" = {
+        "context.modules" = [
+          {
+            name = "libpipewire-module-filter-chain";
+            args = {
+              "node.description" = "Noise Canceling source";
+              "media.name" = "Noise Canceling source";
+              "filter.graph" = {
+                nodes = [
+                  {
+                    type = "ladspa";
+                    name = "rnnoise";
+                    plugin = "${pkgs.rnnoise-plugin}/lib/ladspa/librnnoise_ladspa.so";
+                    label = "noise_suppressor_mono";
+                    control = {
+                      "VAD Threshold (%)" = 85.0;
+                      "VAD Grace Period (ms)" = 200;
+                      "Retroactive VAD Grace (ms)" = 0;
+                    };
+                  }
+                ];
+              };
+              "capture.props" = {
+                "node.name" = "capture.rnnoise_source";
+                "node.passive" = true;
+                "audio.rate" = 48000;
+              };
+              "playback.props" = {
+                "node.name" = "rnnoise_source";
+                "media.class" = "Audio/Source";
+                "audio.rate" = 48000;
+              };
+            };
+          }
+        ];
+      };
+    };
+
+    wireplumber.extraConfig = {
+      "99-auto-connect-rnnoise.lua" = {
+        text = ''
+          table.insert(links, {
+            out_node = "alsa_input.usb-Focusrite_Scarlett_Solo_4th_Gen_S1YE3VE3790E29-00.analog-surround-40",
+            in_node  = "capture.rnnoise_source",
+          })
+        '';
+      };
+    };
+  };
+}
---
+++ hosts/nixos/demon/pterodactyl/panel.nix
@@ -0,0 +1,78 @@
+{
+  pkgs,
+  config,
+  nix-secrets,
+  inputs,
+  lib,
+  ...
+}: let
+  pterodactyl-addons = inputs.pterodactyl-addons;
+in {
+  services.pterodactyl.panel = {
+    enable = true;
+    ssl = true;
+    blueprint = {
+      enable = false;
+      extensions = {
+        "modpack-downloader" = {
+          name = "modpack-downloader";
+          version = "1.0.0";
+          source = "${pterodactyl-addons}/modpack-installer.zip";
+        };
+      };
+    };
+    users = {
+      primary = {
+        email = config.hostSpec.email.user;
+        username = config.hostSpec.username;
+        firstName = config.hostSpec.username;
+        lastName = "G";
+        passwordFile = config.sops.secrets.pterodactylAdminPassword.path;
+        isAdmin = true;
+      };
+    };
+    locations = {
+      uk = {
+        short = "uk";
+        long = "United Kingdom";
+      };
+    };
+  };
+
+  services.nginx = {
+    virtualHosts."pterodactyl-local.addg0.com" = {
+      useACMEHost = "addg0.com";
+      forceSSL = true;
+
+      root = "${config.services.pterodactyl.panel.dataDir}/public";
+      locations."/" = {
+        index = "index.php";
+        tryFiles = "$uri $uri/ /index.php?$query_string";
+      };
+      locations."~ \\.php$" = {
+        extraConfig = ''
+          include ${pkgs.nginx}/conf/fastcgi_params;
+          fastcgi_pass unix:/run/phpfpm/pterodactyl.sock;
+          fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+        '';
+      };
+    };
+  };
+
+  networking.hosts = {
+    "127.0.0.1" = ["pterodactyl-local.addg0.com"];
+  };
+
+  sops.secrets = {
+    pterodactylAdminPassword = {
+      sopsFile = "${nix-secrets}/secrets/pterodactyl/secrets.yaml";
+      mode = "0400";
+      owner = "root";
+    };
+    judePassword = {
+      sopsFile = "${nix-secrets}/secrets/pterodactyl/users.yaml";
+      mode = "0400";
+      owner = "root";
+    };
+  };
+}
