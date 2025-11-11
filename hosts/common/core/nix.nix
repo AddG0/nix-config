61b4662e6d693c2829c92ccf161aaf048e95f12f
addg <addgamer09@gmail.com>
2025-11-11 12:37:29 +0800

    refactor: moved things around

--- hosts/common/core/nix.nix
+++ hosts/common/core/nix.nix
@@ -31,6 +31,7 @@
       extra-nix-path = "nixpkgs=flake:nixpkgs";
       substituters = [
         "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
         "https://anyrun.cachix.org"
         "https://nix-gaming.cachix.org"
         "https://nixpkgs-wayland.cachix.org"
@@ -38,6 +39,7 @@
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
         "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
         "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
