c6d847423d7f4d4b6b44707f896ab06b09f51aa6
addg <addgamer09@gmail.com>
2025-11-11 14:29:34 +0800

    feat: added auto complete for kubectl cloud shell

--- home/common/optional/development/virtualization/kubernetes.nix
+++ home/common/optional/development/virtualization/kubernetes.nix
@@ -1,8 +1,42 @@
 {pkgs, ...}: let
-  kubectl-cloud-shell = pkgs.writeShellApplication {
+  kubectl-cloud-shell-script = pkgs.writeShellApplication {
     name = "kubectl-cloud-shell";
     runtimeInputs = [ pkgs.kubectl ];
     text = builtins.readFile ./kubectl-cloud-shell.sh;
+  };
+
+  kubectl-cloud-shell-completion = pkgs.writeTextFile {
+    name = "kubectl-cloud-shell-completion";
+    destination = "/share/zsh/site-functions/_kubectl-cloud-shell";
+    text = ''
+      #compdef kubectl-cloud-shell
+
+      _kubectl-cloud-shell() {
+        local context state line
+        typeset -A opt_args
+
+        _arguments -C \
+          '--bare[Use bare mode with minimal zsh setup instead of home-manager]' \
+          '*:: :->kubectl_args'
+
+        if [[ $state == kubectl_args ]]; then
+          # Prepend "kubectl run pod-name" to simulate kubectl run completion
+          words=( kubectl run pod-name "''${words[@]}" )
+          # Adjust CURRENT: add 3 for the 3 new words we prepended
+          (( CURRENT = CURRENT + 3 ))
+
+          # Restart completion with the simulated command line
+          _normal
+        fi
+      }
+
+      _kubectl-cloud-shell "$@"
+    '';
+  };
+
+  kubectl-cloud-shell = pkgs.symlinkJoin {
+    name = "kubectl-cloud-shell";
+    paths = [ kubectl-cloud-shell-script kubectl-cloud-shell-completion ];
   };
 in {
   home.packages = with pkgs; [
