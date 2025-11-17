59c97914d7af77cacb36c7f6309c5c96c849227e
addg <addgamer09@gmail.com>
2025-11-17 20:28:35 +0800

    feat: added bakkesmod config support

--- home/common/optional/gaming/bakkesmod.nix
+++ home/common/optional/gaming/bakkesmod.nix
@@ -5,5 +5,14 @@
     plugins = [
       pkgs.bakkesmod-plugins.ingamerank
     ];
+    config = {
+      gui.scale = 1.5;
+      ranked = {
+        showRanks = true;
+        showRanksCasual = true;
+        showRanksCasualMenu = true;
+        showRanksMenu = true;
+      };
+    };
   };
 }
